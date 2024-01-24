f1050f5e30376a73dcd97d2d53f1aec0b7590d8b
viktorija <viktorija.kotaite@gmail.com>
2024-01-24 15:48:10 +0200

    routes login, register, header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Header from "./components/layout/Header";
+
 export default function App() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-semibold mb-10">App</h1>
-      <p>App</p>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </div>
   );
 }
