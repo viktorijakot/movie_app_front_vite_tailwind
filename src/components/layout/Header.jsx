e739cf17a20d1d425835425d23e845b56162fb0d
viktorija <viktorija.kotaite@gmail.com>
2024-01-24 16:08:34 +0200

    header content

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,48 @@
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../store/AuthCtxProvider";
+
 function Header() {
-  return <div>Header</div>;
+  const { isUserLoggedIn, logout } = useAuthContext();
+  const navigate = useNavigate();
+  function handleLogout() {
+    logout();
+    navigate("/login");
+  }
+  return (
+    <div className="bg-slate-300">
+      <header className="container flex justify-between items-center">
+        <Link to="/home" className="text-2xl font-semibold py-4">
+          LOGO
+        </Link>
+        <nav>
+          <NavLink
+            className={"px-4 py-2 hover:bg-slate-500 hover:text-white"}
+            to={"/home"}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={"px-4 py-2 hover:bg-slate-500 hover:text-white"}
+            to={"/login"}
+          >
+            Login
+          </NavLink>
+          <NavLink
+            className={"px-4 py-2 hover:bg-slate-500 hover:text-white"}
+            to={"/register"}
+          >
+            Register
+          </NavLink>
+          <button
+            onClick={handleLogout}
+            className={"px-4 py-2 hover:bg-slate-500 hover:text-white"}
+          >
+            Logout
+          </button>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
