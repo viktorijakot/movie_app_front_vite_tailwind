ac1f0e3783182ed160f8ce729b8b853a5eb87b27
viktorija <viktorija.kotaite@gmail.com>
2024-01-24 16:01:52 +0200

    browser router, authctx added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-// import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.scss";
+import { BrowserRouter } from "react-router-dom";
+import AuthCtxProvider from "./store/AuthCtxProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <App />
+  <BrowserRouter>
+    <AuthCtxProvider>
+      <App />
+    </AuthCtxProvider>
+  </BrowserRouter>
   // </React.StrictMode>,
 );
